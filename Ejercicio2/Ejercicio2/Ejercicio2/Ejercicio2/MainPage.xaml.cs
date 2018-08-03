2cccb5d6c3cbe83e46014397043d4aa18fd5d385
DayannaGarcia <yayisgarcia15@gmail.com>
2018-08-02 20:31:50 -0500

    picker

---
+++ Ejercicio2/Ejercicio2/Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void Handle_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var elementoSeleccionado = picker.SelectedItem as string;
+            DisplayAlert("Seleccionado",elementoSeleccionado,"Ok");
+        }
+    }
+}
