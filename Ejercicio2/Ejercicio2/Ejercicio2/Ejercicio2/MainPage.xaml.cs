126af03b3f088cb786b6cebc6cd48791a642a799
DayannaGarcia <yayisgarcia15@gmail.com>
2018-08-02 20:45:50 -0500

    ProgressBar

--- Ejercicio2/Ejercicio2/Ejercicio2/Ejercicio2/MainPage.xaml.cs
+++ Ejercicio2/Ejercicio2/Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -19,5 +19,18 @@ namespace Ejercicio2
             var elementoSeleccionado = picker.SelectedItem as string;
             DisplayAlert("Seleccionado",elementoSeleccionado,"Ok");
         }
+        void btnSimular_Clicked(object sender, System.EventArgs e)
+        {
+            var progreso = progressNum.Progress;
+
+            if (progreso == 1)
+            {
+                progressNum.ProgressTo(.1, 250, Easing.SpringOut);
+            }
+            else
+            {
+                progressNum.ProgressTo(1,300,Easing.Linear);
+            }
+        }
     }
 }
